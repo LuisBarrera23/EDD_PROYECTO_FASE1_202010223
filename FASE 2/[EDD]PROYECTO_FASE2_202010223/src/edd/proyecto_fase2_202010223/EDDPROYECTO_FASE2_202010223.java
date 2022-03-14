e01b47ca5c42fd2aa0fc3bac2cc9e1289a38c340
LuisBarrera23 <78067073+LuisBarrera23@users.noreply.github.com>
2022-03-14 01:27:10 -0600

    avance

--- FASE 2/[EDD]PROYECTO_FASE2_202010223/src/edd/proyecto_fase2_202010223/EDDPROYECTO_FASE2_202010223.java
+++ FASE 2/[EDD]PROYECTO_FASE2_202010223/src/edd/proyecto_fase2_202010223/EDDPROYECTO_FASE2_202010223.java
@@ -2,7 +2,8 @@ package edd.proyecto_fase2_202010223;
 public class EDDPROYECTO_FASE2_202010223 {
 
     public static void main(String[] args) {
-
+        Login ventana= new Login();
+        ventana.setVisible(true);
     }
     
 }
